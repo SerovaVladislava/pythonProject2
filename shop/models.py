d36ce039cbd344366c4bb0e2766a1cdc91267a84
Владислава <vladislava.serova.98@gmail.com>
2022-01-20 13:51:46 +0500

    Заказы

--- shop/models.py
+++ shop/models.py
@@ -18,7 +18,7 @@ class Section(models.Model):
      def __str__(self):
          return self.title
 
-     class Product(models.Model):
+class Product(models.Model):
          sections = models.ForeignKey('Section', on_delete=models.SET_NULL, null=True, verbose_name='Раздел')
          title = models.CharField(max_length=70, verbose_name='Название')
          image = models.ImageField(upload_to='images', verbose_name='Изображение')
@@ -49,7 +49,7 @@ class Section(models.Model):
     def __str__(self):
         return '{0} ({1})'.format(self.title, self.section.title) #Кто я? (Боевики)
 
-    class Discount(models.Model):
+class Discount(models.Model):
         code = models.CharField(max_length=10, verbose_name='Код купона')
         value = models.IntegerField(
             validators = [MinValueValidator(1), MaxValueValidator(100)],
@@ -60,10 +60,35 @@ class Section(models.Model):
         verbose_name = 'Скидка'
         verbose_name_plural = 'Скидки'
 
+    def __str__(self):
+        return self.code + ' (' + str(self.value) + '%)' #ABC (25%)
 
-def __str__(self):
-    return self.code + ' (' + str(self.value) + '%)' #ABC (25%)
+class Order(models.Model): #Заказ
+    need_delivery = models.BooleanField(verbose_name='Необходима доставка')
+    discount = models.ForeignKey(Discount, verbose_name='Скидка', on_delete=models.SET_NULL, null=True)
+    name = models.CharField(max_length=70, verbose_name='Имя')
+    phone = models.CharField(max_length=70, verbose_name='Телефон')
+    email = models.EmailField()
+    address = models.TextField(verbose_name='Адрес',blank=True)
+    notice = models.TextField(blank=True, verbose_name='Примечание к заказу')
+    date_order = models.DateTimeField(auto_now_add=True, verbose_name='Дата заказа')
+    date_send = models.DateTimeField(null=True, blank=True, verbose_name='Дата отправки')
 
+    STATUSES = [ #список статусов
+        ('NEW', 'Новый заказ'),
+        ('APR', 'Подтвержден'),
+        ('PAY', 'Оплачен'),
+        ('CNL', 'Отменен'),
+    ]
 
+    status = models.CharField(choices=STATUSES, max_length=3, default='NEW', verbose_name='Статус')
+
+    class Meta:
+        ordering = ['-date_order'] #сортировка заказов, начинающаяся со самых свежих заказов
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+    def __str__(self):
+        return 'ID: ' + str(self.id)
 
          
